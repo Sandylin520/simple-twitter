2139bc3ca00b253f95e349982a83e6e942a4b07e
sansan <b810521@gmail.com>
2018-07-21 18:44:25 +0800

    set up routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,33 @@ Rails.application.routes.draw do
 
   # 請依照專案指定規格來設定路由
 
+  #前台
+    root "tweets#index"
+    resources :tweets, only: [:index, :create] do
+       resources :replies, only: [:create, :index]
+
+        member do
+          post :like
+          post :unlike
+        end
+    end
+
+    resources :users, only: [:show, :edit] do
+       member do
+         get :tweets
+         get :followings
+         get :followers
+         get :likes
+       end
+    end
+
+    resources :followships, only: [:create, :destroy]
+
+    #admin後台
+    namespace :admin do
+      resources :tweets, only: [:index,:destroy]
+      resources :users, only: [:index]
+      root "tweets#index"
+    end
+
 end
