cd8a02e0ff1f1d5483c844826211425676c8456e
sansan <b810521@gmail.com>
2018-07-28 17:12:27 +0800

    use devise gem to build sign up function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user! #Devise 提供的方法
   def tweets
   end
 
@@ -16,9 +16,4 @@ class UsersController < ApplicationController
   def followers
     @followers # 基於測試規格，必須講定變數名稱
   end
-
-  def likes
-    @likes # 基於測試規格，必須講定變數名稱
-  end
-
 end
