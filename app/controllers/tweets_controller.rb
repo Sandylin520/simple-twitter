dad39261d6452c069306bb785e58e43a713b7d6f
sansan <b810521@gmail.com>
2018-08-18 22:01:19 +0800

    create a form table for user to enter  tweet description on tweet index view page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @users = User.all
+    @tweet = Tweet.new
   end
 
   def create
@@ -14,6 +15,13 @@ class TweetsController < ApplicationController
   end
 
   def unlike
+  end
+
+
+private
+
+  def tweet_params
+   params.require(:tweet).permit(:description)
   end
 
 end
