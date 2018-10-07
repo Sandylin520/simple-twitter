8547d42b84fd1185891dbcbe124dd35a53afb2ef
sansan <b810521@gmail.com>
2018-10-08 06:39:56 +0800

    create reply controller create action and set reply's comment cannot be blank in reply model

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,13 +1,28 @@
 class RepliesController < ApplicationController
   def index
     @tweet = Tweet.find(params[:tweet_id])
-    @replies = Reply.all
-    @user = @tweet.user
-    @reply = Reply.new
+    @replies = @tweet.replies
+    @user = @tweet.user #for view中button 使用
+    @reply = Reply.new  #for form_for 創建reply表單使用
   end
 
   def create
-  
+    @tweet = Tweet.find(params[:tweet_id])#確認評論屬於哪一個tweet
+    @reply = @tweet.replies.build(reply_params)#建立屬於 ＠tweet的評論,評論內容來自表單傳入的資料
+    @reply.user = current_user
+    if  @reply.save #將評論存入資料庫
+      flash[:notice] = "Reply is successfully created"
+      redirect_to tweet_replies_path(@tweet)#回到reply index頁面
+    else
+      flash[:alert] ="Reply cannot be blank"
+      redirect_to tweet_replies_path(@tweet)
+    end
   end
 
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,5 @@
 class Reply < ApplicationRecord
+  validates_presence_of :comment #comment 值不可以為空白
   belongs_to :user
   belongs_to :tweet,counter_cache: true #replies_count 有在tweet底下
 end
