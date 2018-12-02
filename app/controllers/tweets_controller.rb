bfdeeccccff036a2cef0c7fd6c205c359bbdf715
sansan <b810521@gmail.com>
2018-12-02 18:13:45 +0800

    update tweets controller index action enable to sort @tweets & @users / apply reply_and_like_button shared view to replies index view & tweets index view & users likes view & users tweets view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,8 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @tweets = Tweet.all # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @users = User.all
+    @tweets = Tweet.order(created_at: :desc)
+    @users = User.order(followers_count: :desc).limit(10)# 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweet = Tweet.new #form_for表單需要有物件傳入 按鈕纔會顯示create
   end
 
