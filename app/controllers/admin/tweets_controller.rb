5176222e2d28aecf746642553a29b82466c49487
sansan <b810521@gmail.com>
2018-12-16 22:30:29 +0800

    admin can see all uses' names & intros & replies

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,8 +1,12 @@
 class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
-  
+
   def index
+    @tweets = Tweet.all.includes(:replies,:user)
+    #使用rails include方法,一次將需要的資料查好
+    #回傳所有tweet
+    #tweets ,replies,user內容皆已在本次查詢,之後不會產生額外的查詢比數
   end
 
   def destroy
